7a3106235438c9b433e5f64637c146dd9eb38ce5
Tyler Corbett <thetylercorbett@gmail.com>
2019-12-31 17:31:01 -0800

    basic polish added for next release

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './index.css';
 import ClipsContainer from './components/containers/clipscontainer/ClipsContainer';
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
-    <div>
+    <div className="App">
       <Header />
       <ClipsContainer />
+      <Footer/>
     </div>
   );
 };
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className="Footer">
+      <h3>Made by <a rel="noopener noreferrer" target="_blank" href="https://tylercorbett.me/">Tyler Corbett</a></h3>
+    </footer>
+  );
+};
+
+export default Footer;
